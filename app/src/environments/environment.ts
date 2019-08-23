4d07da06dab7a673d85bda9504dcf24fb9b04cc7
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-08-22 17:44:31 -0700

    env variables

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  omdb: {
+	apiUrl: 'http://www.omdbapi.com',
+	apiKey: 'e1410c2b'
+  }
 };
 
 /*
