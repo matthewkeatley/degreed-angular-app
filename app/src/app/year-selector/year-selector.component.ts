d754d38e20c9e90555d80833fa9c36a7e079dc92
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-08-22 17:44:24 -0700

    year selector

---
+++ app/src/app/year-selector/year-selector.component.ts
@@ -0,0 +1,16 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-year-selector',
+  templateUrl: './year-selector.component.html',
+  styleUrls: ['./year-selector.component.scss']
+})
+export class YearSelectorComponent {
+	@Output() notify = new EventEmitter();
+
+	form = new FormGroup({
+		decade: new FormControl('1990')
+	});
+
+}
