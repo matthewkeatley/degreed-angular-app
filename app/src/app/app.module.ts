d81d406636c23a447d3dfd976c3745c21dfe2b1b
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-08-22 17:43:45 -0700

    modules

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { YearSelectorComponent } from './year-selector/year-selector.component';
+import { DecadePipe } from './decade.pipe';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MovieListComponent,
+    YearSelectorComponent,
+    DecadePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	HttpClientModule,
+	FormsModule,
+	ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
